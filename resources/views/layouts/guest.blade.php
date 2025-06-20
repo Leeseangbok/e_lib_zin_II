f74c6c89d3300b3a174d4a86226f8a7df3d45d7c
Leeseangbok <leeseangbok15@gmail.com>
2025-06-20 14:23:38 +0700

    Done User and admin

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,27 +9,33 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jersey+15&family=Joti+One&family=Winky+Sans:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased overflow-hidden m-0 p-0 min-h-screen">
     <div class="flex flex-col sm:flex-row w-screen min-h-screen">
         <!-- Left Side (Hidden on small screens) -->
-        <div class="hidden sm:flex w-full sm:w-1/2 h-64 sm:h-screen bg-purple-800/90 text-white px-6 py-8 flex-col items-center shadow-lg relative overflow-hidden">
+        <div class="hidden sm:flex w- winky sm:w-1/2 h-64 sm:h-screen bg-purple-800/90 text-white px-6 py-8 flex-col items-center shadow-lg relative overflow-hidden">
             <!-- Background image -->
             <div class="absolute inset-0 z-0 bg-cover bg-center opacity-50" style="background-image: url('/background.png');"></div>
             <div class="flex flex-col items-start justify-between h-full w-full relative z-10">
-                <div class="top-10 left-10">
-                    <h1 class="text-4xl font-bold mb-4">Welcome to E-Lib</h1>
+                <div class="flex items-center mb-6">
+                    <img src="/e_lib_logo.png" alt="E-Lib Logo" class="w-16 h-16 rounded-full mr-4">
+                    <h1 class="text-4xl joti font-bold">E-Lib</h1>
+                </div>
+                <div class="mt-20">
+                    <h1 class="text-7xl font-bold mb-4">Welcome to E-Lib</h1>
                     <p class="text-lg">Your digital library awaits.</p>
                 </div>
                 <div class="text-sm">
-                    <p><strong>Email Support:</strong> support@elib.com</p>
-                    <p><strong>Phone Support:</strong> +1 (234) 567-890</p>
+                    <p><strong>Email Support:</strong> leeseangbok15@gmail.com</p>
+                    <p><strong>Phone Support:</strong> +855 97 536 0168</p>
                 </div>
             </div>
         </div>
